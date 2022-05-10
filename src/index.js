b7b7b22739016dcdda1530234ae037dbf93a8c15
unknown <saradawsone@gmail.com>
2022-05-10 15:18:26 -0700

    click and mouseover added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './less/index.less'
 
 // Your code goes here!
+// click
+const h1 = document.querySelector('h1.logo-heading');
+console.log(h1)
+h1.onclick = function () {
+    console.log('you clicked');
+}
+h1.addEventListener('click', function(evt) {
+    console.log(`
+        event type: ${evt.target}
+        event target: ${evt.target.nodeName}
+    `)
+})
+
+// mouseover
+h1.addEventListener('mouseover', function(evt) {
+    evt.target.style.color = 'purple';
+})
